53988b41175dc7d44e89c492273d5cb75e7e900a
android156 <n_degtyarev@mail.ru>
2021-08-16 19:23:22 +0300

    дз 1 с кнопками

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 "use strict";
+function calcCtoF() {
+    let val_tempC = +document.getElementById('tempC').value;
+    let val_tempF = val_tempC * 9 / 5 + 32;
+    document.getElementById('tempF').value = val_tempF.toFixed(1);
+}
+function calcCtoC() {
+    let val_tempF = +document.getElementById('tempF').value;
+    let val_tempC = (val_tempF - 32) * 5 / 9;
+    document.getElementById('tempC').value = val_tempC.toFixed(1);
+}

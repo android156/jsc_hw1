ba10abcd0e460afb91b4db41c77cb6983f0c2acf
android156 <n_degtyarev@mail.ru>
2021-08-18 17:31:28 +0300

    Задание 3

--- app.js
+++ app.js
@@ -238,6 +238,7 @@ function makeDeposit() {
 
 //Начинаем ДЗ№3
 
+// Задание 1
 /* С помощью цикла for написать алгоритм для вывода чисел (выводите в консоль, с помощью console.log) от 0 до 10 включительно, чтобы результат выглядел так:
 0 – это ноль
 1 – нечетное число
@@ -245,6 +246,7 @@ function makeDeposit() {
 3 – нечетное число
 …
 10 – четное число */
+
 
 function getNumberDescription(number) {
     if (number % 2) {
@@ -259,3 +261,58 @@ for (let i = 0; i <= 10; i++) {
     number_description = (i == 0) ? "это ноль" : getNumberDescription(i);
     console.log(`${i} - ${number_description}`);
 }
+
+// Задание 2
+
+/* 
+Выведите в консоль значения, указанные рядом с комментариями: */
+const post = {
+    author: "John", //вывести этот текст 
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+                likes: 10,
+                dislikes: 2 //вывести это число 
+            }
+        },
+        {
+            userId: 5, //вывести это число 
+            userName: "Jane",
+            text: "lorem ipsum 2", //вывести этот текст 
+            rating: {
+                likes: 3,
+                dislikes: 1
+            }
+        },
+    ]
+};
+
+// Да без проблем
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+
+// Задание 3
+
+/* Перед вами находится массив с продуктами, сегодня распродажа и вам нужно на каждый
+товар применить скидку 15%, можете использовать метод forEach */
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];

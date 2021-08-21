b4282f93cfc0d6c19869346b0a2ddd15523214fa
android156 <n_degtyarev@mail.ru>
2021-08-21 16:14:54 +0300

    1.1 done

--- app.js
+++ app.js
@@ -453,3 +453,39 @@ function numberIntoObj(random_number) {
 }
 
 console.log(numberIntoObj(random_number));
+
+// Задание 1.1
+
+/* Сделайте в стиле es5, а затем в стиле es6 (по аналогии из дополнительных видео -> 3 примеры 
+    наследования -> типы на es5 и es6), конструктор Product, который принимает параметры name 
+    и price, сохраните их как свойства объекта. Также объекты типа Product должны иметь метод 
+    make25PercentDiscount, который будет уменьшать цену в объекте на 25%. Имейте в виду, что 
+    метод make25PercentDiscount не должен быть внутри функции-конструктора, и также не нужно 
+    создавать отдельный объект-прототип (как объект transport в уроке). */
+
+// Тут все элементарно
+
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+    Product.prototype.make25PercentDiscount = function () {
+        this.price *= 0.75;
+    }
+}
+
+class Product2 {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    make25PercentDiscount() {
+        this.price *= 0.75;
+    }
+}
+
+const product_ES5 = new Product("Веник", 100);
+const product_ES6 = new Product2("Метелка", 200);
+product_ES5.make25PercentDiscount();
+product_ES6.make25PercentDiscount();
+console.log(product_ES5.price); //75
+console.log(product_ES6.price); //150
